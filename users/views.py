abfba7734eb0b366019025c934a0c07433673bbc
박종음 <qkrwhddma@naver.com>
2019-12-04 21:46:37 +0900

    #20 Make it all Beautiful

--- users/views.py
+++ users/views.py
@@ -32,11 +32,6 @@ class SignUpView(FormView):
     template_name = 'users/signup.html'
     form_class = forms.SignUpForm
     success_url = reverse_lazy('core:home')
-    initial = {
-        'first_name': 'Park',
-        'last_name': 'jongeum',
-        'email': 'test@naver.com'
-    }
 
     def form_valid(self, form):
         form.save()
